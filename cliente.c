2a25f9ba1fc383d63841f6df020866782aaff058
alejandra <alejandra@cc.ac.labf.usb.ve>
2016-06-20 00:31:30 -0400

    Se agregaron sockets

--- cliente.c
+++ cliente.c
@@ -10,10 +10,14 @@
 int main(int argc, char *argv[])
 {
 	int serverPort;
+	int sockfd;
 	int identificador;
+	int addr_len,numbytes; /* conteo de bytes a escribir */ 
 	char *operacion;
 
+	struct sockaddr_in their_addr; /* almacenara la direccion IP y numero de puerto del servidor */ 
 	struct hostent *direccionDestino; 
+
 
 	/* code */
 	if (argc != 9){
@@ -74,5 +78,33 @@ int main(int argc, char *argv[])
 
 
 	}
+
+	/* Creamos el socket */ 
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) { 
+		perror("socket"); 
+		exit(2); 
+	} 
+
+
+	/* a donde mandar */ 
+	their_addr.sin_family = AF_INET; /* usa host byte order */ 
+	their_addr.sin_port = htons(serverPort); /* usa network byte order */ 
+	their_addr.sin_addr = *((struct in_addr *)direccionDestino->h_addr); 
+	bzero(&(their_addr.sin_zero), 8); /* pone en cero el resto */ 
+
+	char hola[100];
+	scanf("%[^\n]s :",hola);
+	
+	/* enviamos el mensaje */ 
+	if ((numbytes=sendto(sockfd,hola,strlen(hola),0,
+		(struct sockaddr *)&their_addr,sizeof(struct sockaddr))) == -1) { 
+		perror("sendto"); 
+		exit(2); 
+	} 
+
+	/* cierro socket */ 
+	close(sockfd); 
+
+
 	return 0;
 }
